78d75b37aae378dd4240434084f38d748bffc760
xboarder56 <garrettbeasley@icloud.com>
2015-01-29 13:26:15 -0800

    New Babyname pairprogramming

---
+++ src/pp/babyname/finder/NameFindDriver_NIO.java
@@ -0,0 +1,61 @@
+package pp.babyname.finder;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class NameFindDriver_NIO 
+{
+	private static final Scanner console = new Scanner(System.in);
+	private static boolean matchfound, matchfound2, matchfound3;
+
+	public static void main(String[] args)
+	{
+		try 
+		{
+			String content = new String(Files.readAllBytes(Paths.get("babynames.txt")));
+			
+			
+			/*Tell the user to pick a selection between 1-4*/
+			System.out.println("Please enter the name to find in the list: ");
+			String name1 = console.nextLine(); //clears the prompt (to skip enter key)
+			
+			
+			/*Tell the user to pick a selection between 1-4*/
+			System.out.println("Please enter the name to find in the list: ");
+			String name2 = console.nextLine(); //clears the prompt (to skip enter key)
+			
+			/*Tell the user to pick a selection between 1-4*/
+			System.out.println("Please enter the name to find in the list: ");
+			String name3 = console.nextLine(); //clears the prompt (to skip enter key)
+			
+			if(content.contains(name1))
+			{
+				matchfound = true;
+			}
+			
+			if(content.contains(name2))
+			{
+				matchfound2 = true;
+			}
+			if(content.contains(name3))
+			{
+				matchfound3 = true;
+			}
+			
+			System.out.println(name1 + " is found? "+ matchfound);
+			System.out.println(name2 + " is found? "+ matchfound2);
+			System.out.println(name3 + " is found? "+ matchfound3);
+		} 
+		catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+
+
+}
